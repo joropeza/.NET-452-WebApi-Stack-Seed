05bd1f146657fae082518bd888c272599ec36b86
joropeza <jon@zacsi.com>
2015-04-20 21:59:30 -0700

    added services layer

---
+++ Services_Layer/Services/ProductsService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services_Layer.Services
+{
+    public class ProductsService : ICoreService
+    {
+        public IServiceResponseMessage List(ServiceResponseMessage<int> newRequest)
+        {
+            newRequest.RequestObject = 5;
+
+            this.request = newRequest;
+
+            return request;
+
+        }
+
+    }
+}
